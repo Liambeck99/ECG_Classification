2a0b341e9c843db89e29faaf336179eb7b8ea9ea
Liambeck99 <Lj.beckingham99@gmail.com>
2021-01-28 12:30:40 +0000

    removed clutter and addeded reference to source of ECG calculations to cardiacSimulation code

--- delayDifferentials.py
+++
@@ -1,5 +0,0 @@
-# creation of simulated ECG signals from delay differential equations
-# proposed equations from Cheffer and Savi, paper reference : 
-# A. Cheffer and M. A. Savi. Random effects inducing heart pathological dynamics:
-# An approach based on mathematical models. Biosystems, 196:104177, 2020.
-
